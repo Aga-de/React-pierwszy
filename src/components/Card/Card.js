f494f5f8fca24774c898465537dd651dcee4238e
Aga Daniuk <agdani@wp.pl>
2021-01-26 14:21:15 +0100

    12.5 Add cards & icons

---
+++ src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
+
+
+class Card extends React.Component {
+    static propTypes = {
+        title: PropTypes.string, 
+    }
+    render(){
+        return (
+            <section className={styles.component}>
+                <div>{ReactHtmlParser(this.props.title)}</div>
+            </section>
+        )
+    }
+}
+
+export default Card;
