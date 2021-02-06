07b84655b9ec766baea38e0760262eb350bb05b2
Aga Daniuk <agdani@wp.pl>
2021-02-06 17:44:24 +0100

    Add cardMove - problem to drag in same column

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,18 +2,30 @@ import React from 'react';
 import styles from './Card.scss';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
+import {Draggable} from 'react-beautiful-dnd';
 
 
 class Card extends React.Component {
     static propTypes = {
       title: PropTypes.string, 
+      id: PropTypes.string,
+      index: PropTypes.number,
     }
     render(){
-      const {title} = this.props;
+      const {title, id, index} = this.props;
       return (
-        <section className={styles.component}>
-          <div>{ReactHtmlParser(title)}</div>
-        </section>
+        <Draggable draggableId={id} index={index}>
+          {(provided) => (
+            <article
+              className={styles.component}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              ref={provided.innerRef}
+            >
+              <div>{ReactHtmlParser(title)}</div>
+            </article>
+          )}
+        </Draggable>
       );
     }
 }
